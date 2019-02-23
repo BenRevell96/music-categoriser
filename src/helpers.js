dcf49710153c37d006f86d72cc835ad20cd56456
Ben Revell <benrevell.96@gmail.com>
2019-02-23 17:13:47 +0000

    wip

--- src/helpers.js
+++ src/helpers.js
@@ -1,36 +1,15 @@
 const fs = require('fs')
-const audioMetaData = require('audio-metadata')
 
 const { fileTypes } = require('./Constants.js')
 
-// todo these can all be sync 🚨🚨🚨🚨🚨
-
-const extractAudioFiles = fileName => {
-  return fileTypes.some(v => fileName.indexOf(v) >= 0)
-}
+const extractAudioFiles = fileName =>
+  fileTypes.some(v => fileName.indexOf(v) >= 0)
 
 const createAudioFileList = folderPath =>
-  new Promise(resolve => {
-    fs.readdir(`${folderPath}`, { withFileTypes: true }, (err, files) => {
-      if (err) {
-        console.log(err)
-      }
-      return resolve(files.filter(fileName => extractAudioFiles(fileName)))
-    })
-  })
+  fs
+    .readdirSync(`${folderPath}`, { withFileTypes: true })
+    .filter(fileName => extractAudioFiles(fileName))
 
-const readAudioFile = (folderPath, file) =>
-  new Promise(resolve => {
-    fs.readFile(`${folderPath}/${file}`, (err, data) => {
-      if (err) {
-        console.log(err)
-      }
-      console.log(audioMetaData.ogg(data))
-      // console.log(data)
-      resolve(data)
-    })
-  })
 module.exports = {
-  createAudioFileList,
-  readAudioFile,
+  createAudioFileList
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const os = require('os')
-
-const { createAudioFileList, readAudioFile } = require('./helpers.js')
+const { createAudioFileList } = require('./helpers.js')
+const ffmetadata = require('ffmetadata')
 
 const folderPath = process.argv[2] || `${os.homedir()}/Music/`
 
-const eligibleFiles = async () => {
-  const files = await createAudioFileList(folderPath)
-
+const eligibleFiles = () => {
+  const files = createAudioFileList(folderPath)
   for (i = 0; i < files.length; i++) {
-    readAudioFile(folderPath, files[i])
+    const filePath = `${folderPath}/${files[i]}`.toString()
+
+    ffmetadata.read(filePath, (err, data) => {
+      console.log(`${folderPath}/${files[i]}`)
+
+      if (err) {
+        console.log('Error reading metadata', err)
+      } else console.log(data)
+    })
   }
 }
 
